ac983a2ce5090a5d01a24671c041244c7dd0b36e
Chima Precious <ositachima.co@gmail.com>
2024-05-14 21:23:49 +0000

    move database config into project

--- database/database.dart
+++ database/database.dart
@@ -1,10 +1,10 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:quiz_app/src/config.dart' as config;
 import 'package:quiz_app/src/model/user.dart';
 import 'package:yaroorm/yaroorm.dart';
 
-import './config.dart' as config;
 import 'migrations/2024_05_11_173502_create_users_table.dart' as _m0;
 
 void initializeORM() {
